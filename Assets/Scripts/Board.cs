79603542008852f0ab34aabf02cefd633e939e2e
karelharjono <harjono.karel@gmail.com>
2025-03-13 19:16:40 -0700

    implement spawnPiece to spawn random tile for a given spawnPosition

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,8 +1,46 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
+    public Vector3Int spawnPosition;
     public TetrominoData[] tetrominoes;
 
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        this.activePiece = GetComponentInChildren<Piece>();
 
+        for (int i = 0; i < tetrominoes.Length; i++)
+        {
+            tetrominoes[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int randomIndex = Random.Range(0, tetrominoes.Length);
+        TetrominoData data = tetrominoes[randomIndex];
+
+        this.activePiece.Initialize(this, this.spawnPosition, data);
+        Set(activePiece);
+    }
+
+    public void Set(Piece piece)
+    {
+        for (int i = 0; i < piece.cells.Length; i++)
+        {
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
+
+    
 }
